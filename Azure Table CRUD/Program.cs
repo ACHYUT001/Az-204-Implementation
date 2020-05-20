515627ba2aab0135c05f01bab5b601f22435b19c
acupadhy <achyutpdh249@gmail.com>
2020-05-20 10:06:35 -0400

    Pushing Table Implementation

---
+++ Azure Table CRUD/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace Azure_Table_CRUD
+{
+        class Program
+        {
+            static CloudStorageAccount storageAccount;
+            static CloudTableClient tableclient;
+            static CloudTable employees;
+
+
+
+            static void Main(string[] args)
+            {
+
+                //1. create connection to azure(via storage account) 2. create a table client 3. use table client to fetch the reference to the table
+                storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storageaccountproje8a67;AccountKey=ZeMMGXoAUJpRI/7T9m1wijt3L4jOAX8e56KlkZC1H/gm4LBrflD9cPYdFBeIzi/4wk5gjrkkw518GJ3K5cHf0A==;EndpointSuffix=core.windows.net");
+                tableclient = storageAccount.CreateCloudTableClient();
+                employees = tableclient.GetTableReference("tablecrud");
+
+                InsertOp("John", "Parry", "Sage");
+                InsertOp("Lyra", "Silvertongue", "Leader");
+                InsertOp("Iorek", "Byrnison", "Leader");
+
+                QueryOp();
+
+
+
+
+
+            }
+
+
+            public static void InsertOp(string firstname, string lastname, string designation)
+            {
+                Employees emp = new Employees(firstname, lastname, designation);
+
+                TableOperation insertoperation = TableOperation.Insert(emp);
+                employees.Execute(insertoperation);
+
+            }
+
+            public static void QueryOp()
+            {
+                TableQuery<Employees> query = new TableQuery<Employees>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "Leader"));
+                Console.WriteLine("Here are the name of your Leaders");
+                foreach (Employees e in employees.ExecuteQuery(query))
+                {
+                    Console.WriteLine(e.RowKey);
+                }
+            }
+        }
+
+        public class Employees : TableEntity
+        {
+            public Employees(string firstname, string lastname, string designation)
+            {
+
+                this.PartitionKey = designation;
+                this.RowKey = firstname + " " + lastname;
+
+            }
+
+            public Employees()
+            {
+            }
+        }
+}
